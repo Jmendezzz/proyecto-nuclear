0fbd6adbe1bd59d5d477b580dd87253e37c2f16e
Juan Gerardo <jmendez1025@cue.edu.co>
2023-05-22 21:15:06 -0500

    feat(backend): herency spring jpa added

--- backend/src/main/java/co/edu/cue/proyectonuclear/domain/entities/Professor.java
+++ backend/src/main/java/co/edu/cue/proyectonuclear/domain/entities/Professor.java
@@ -1,14 +1,22 @@
 package co.edu.cue.proyectonuclear.domain.entities;
 
 import co.edu.cue.proyectonuclear.domain.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "professor")
 public class Professor extends User {
 
-
+    @OneToMany
+    @JoinTable(name = "professor_subjects")
     private List<Subject> subjects;
-    public Professor(Long id, String name, String lastName, Role role, String email, String password) {
-        super(id, name, lastName, role, email, password);
-    }
+
 }
--- backend/src/main/java/co/edu/cue/proyectonuclear/domain/entities/Subject.java
+++ backend/src/main/java/co/edu/cue/proyectonuclear/domain/entities/Subject.java
@@ -1,14 +1,22 @@
 package co.edu.cue.proyectonuclear.domain.entities;
 
 import co.edu.cue.proyectonuclear.domain.enums.Career;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Subject {
+    @Id
     private  Long id;
     private String name;
+
+    @Enumerated(EnumType.STRING)
     private Career career;
     private Integer semester;
     private  Integer credits;
