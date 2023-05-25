b47c882ab96ac003d3e42c6a7b37762f5d76339a
santiCardona <sn09pro@gmail.com>
2023-05-24 23:12:44 -0500

    feat(student): list and find by student ID functionalities

--- backend/src/main/java/co/edu/cue/proyectonuclear/infrastructure/controllers/StudentController.java
+++ backend/src/main/java/co/edu/cue/proyectonuclear/infrastructure/controllers/StudentController.java
@@ -1,4 +1,26 @@
 package co.edu.cue.proyectonuclear.infrastructure.controllers;
 
+import co.edu.cue.proyectonuclear.domain.entities.Student;
+import co.edu.cue.proyectonuclear.services.StudentService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@AllArgsConstructor
 public class StudentController {
+    StudentService studentService;
+    @GetMapping("/students")
+    public List<Student> getAllStudent(){return studentService.getAllStudent();}
+
+    @GetMapping("/students/{id}")
+    public Student getById(@PathVariable Long id){
+        Optional<Student> student = studentService.getStudentById(id);
+        //TODO: Do a validation if the student exists
+        return student.get();
+    }
 }
--- backend/src/main/java/co/edu/cue/proyectonuclear/services/StudentService.java
+++ backend/src/main/java/co/edu/cue/proyectonuclear/services/StudentService.java
@@ -1,4 +1,11 @@
 package co.edu.cue.proyectonuclear.services;
 
+import co.edu.cue.proyectonuclear.domain.entities.Student;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface StudentService {
+    List<Student> getAllStudent();
+    Optional<Student> getStudentById(Long id);
 }
