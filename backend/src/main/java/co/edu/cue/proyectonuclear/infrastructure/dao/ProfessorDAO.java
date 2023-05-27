7ad6c7be21933ba7969b6b0d42dedd8180b7c574
Y00w1 <dgomez1019@cue.edu.co>
2023-05-26 22:05:52 -0500

    feat(professor): ProfessorDTO creation

--- backend/src/main/java/co/edu/cue/proyectonuclear/infrastructure/dao/ProfessorDAO.java
+++ backend/src/main/java/co/edu/cue/proyectonuclear/infrastructure/dao/ProfessorDAO.java
@@ -1,13 +1,14 @@
 package co.edu.cue.proyectonuclear.infrastructure.dao;
 
 import co.edu.cue.proyectonuclear.domain.entities.Professor;
+import co.edu.cue.proyectonuclear.mapping.dtos.CreateProfessorRequestDTO;
 import co.edu.cue.proyectonuclear.mapping.dtos.ProfessorDTO;
 
 import java.util.List;
 
 public interface ProfessorDAO {
     List<ProfessorDTO> getAllProfessors();
-    ProfessorDTO createProfessor(ProfessorDTO professor);
+    ProfessorDTO createProfessor(CreateProfessorRequestDTO professor);
     ProfessorDTO getProfessorById(Long id);
 
 }
--- backend/src/main/java/co/edu/cue/proyectonuclear/mapping/mappers/ProfessorMapper.java
+++ backend/src/main/java/co/edu/cue/proyectonuclear/mapping/mappers/ProfessorMapper.java
@@ -1,6 +1,7 @@
 package co.edu.cue.proyectonuclear.mapping.mappers;
 
 import co.edu.cue.proyectonuclear.domain.entities.Professor;
+import co.edu.cue.proyectonuclear.mapping.dtos.CreateProfessorRequestDTO;
 import co.edu.cue.proyectonuclear.mapping.dtos.ProfessorDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -9,4 +10,7 @@ import org.mapstruct.Mapping;
 public interface ProfessorMapper {
     ProfessorDTO mapFrom(Professor source);
     Professor mapFrom(ProfessorDTO source);
+    @Mapping(target = "password", source = "professorRequestDTO.id")
+    @Mapping(target = "role", expression = "java(co.edu.cue.proyectonuclear.domain.enums.Role.PROFESSOR)")
+    Professor mapFromDTO(CreateProfessorRequestDTO professorRequestDTO);
 }
