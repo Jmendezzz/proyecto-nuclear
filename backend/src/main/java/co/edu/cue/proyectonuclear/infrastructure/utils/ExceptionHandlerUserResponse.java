83ad1823fc296103ad02ceac70a0ad807c76922a
Y00w1 <dgomez1019@cue.edu.co>
2023-05-28 18:47:07 -0500

    fix(professor): Handler

--- backend/src/main/java/co/edu/cue/proyectonuclear/infrastructure/utils/ExceptionHandlerUserResponse.java
+++ backend/src/main/java/co/edu/cue/proyectonuclear/infrastructure/utils/ExceptionHandlerUserResponse.java
@@ -3,12 +3,13 @@ package co.edu.cue.proyectonuclear.infrastructure.utils;
 import co.edu.cue.proyectonuclear.exceptions.UserCreationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDate;
-
+@ControllerAdvice
 public class ExceptionHandlerUserResponse extends ResponseEntityExceptionHandler {
     @ExceptionHandler(UserCreationException.class)
     public final ResponseEntity<ErrorDetail> UserCreationException(Exception ex, WebRequest req){
