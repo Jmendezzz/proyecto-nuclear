22432fb094eddfea705c155b1b8b795ed00509b1
Jmendezzz <jmendez1025@cue.edu.co>
2023-06-01 19:14:25 -0500

    feat(frontend): Sidebar finished

---
+++ frontend/src/components/subject/Subject.jsx
@@ -0,0 +1,16 @@
+import { Flex } from "../../UI/flex/Flex"
+import { Header } from "../../UI/headers/Header"
+
+
+export const Subject = () => {
+    return (
+        <Flex height={"100%"} width={"100%"} direction={"column"} alignItems={"center"} justifyContent={"none"}> 
+            <Header>
+                <h2 style={{fontSize:"60px"}}>ASIGNATURAS</h2>
+            </Header>
+    
+
+        </Flex>   
+    )
+        
+}
