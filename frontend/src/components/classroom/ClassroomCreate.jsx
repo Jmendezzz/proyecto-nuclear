1ce6c9432b555d37f6549107d6be6782688cc140
samuel <samuelsalcedoramirez@gmail.com>
2023-06-04 11:33:31 -0500

    feature(Classroom):Implemneted search

--- frontend/src/components/classroom/ClassroomCreate.jsx
+++ frontend/src/components/classroom/ClassroomCreate.jsx
@@ -35,8 +35,8 @@ const validateForm = (values) => {
 
 const succesResponseAlert = (response) => {
   Swal.fire({
-    title: "Asignatura creada",
-    text: "Se ha creado la asignatura " + response.data.name,
+    title: "Salon creado",
+    text: "Se ha creado el salon" + response.data.name,
     icon: "success",
     confirmButtonColor: "green",
     confirmButtonText: "Aceptar"
@@ -223,119 +223,6 @@ export const ClassroomCreate = () => {
         </Flex>
       </Flex>
     </Flex>
-    //   <Flex
-    //   height={"100%"}
-    //   width={"100%"}
-    //   direction={"column"}
-    //   alignItems={"center"}
-    //   justifyContent={"none"}
-    // >
-    //   <Header>
-    //     <h2 style={{ fontSize: "60px" }}>CREAR SALONES</h2>
-    //   </Header>
-    //   <Flex
-    //     height={"auto"}
-    //     width={"80%"}
-    //     direction={"column"}
-    //     className={style["main-container"]}
-    //     justifyContent={"none"}
-    //     alignItems={"center"}
-    //   >
-    //     <Flex
-    //       justifyContent={"none"}
-    //       alignItems={"center"}
-    //       direction={"column"}
-    //       gap="20px"
-    //       width={"90%"}
-    //       className={style["create-subject-container"]}
-    //     >
 
-
-    //       <Flex
-    //         direction={"column"}
-    //         className={style["create-classroom-container"]}
-    //         height={"auto"}
-    //         alignItems={"none"}
-    //         justifyContent={"none"}
-    //       >
-           
-            
-    //         <label style={{ fontSize: "20px" }}>Nombre</label>
-    //         <Input style={{ height: "10px" }} input={{ onChange: classroomNameValueChangeHandler,onBlur:classroomNameBlurChangeHandler }} ></Input>
-    //         {classroomNameIsInvalid && <p style={{color:"red"}}>El nombre no debe estar vacío</p>}
-    //       </Flex>
-    //       <Flex
-    //         direction={"column"}
-    //         height={"auto"}
-    //         alignItems={"none"}
-    //         justifyContent={"none"}
-    //       >
-            // <label style={{ fontSize: "20px" }}>Ubicacion</label>
-            // <Select
-            //   onChange={selectLocationHandler}
-            //   defaultValue={{ label: location[0].name, value: location[0].value }}
-            //   noOptionsMessage={() => "No se encontraron carreras "}
-            //   className={style.select}
-            //   options={location.map((location) => ({
-            //     label: location.name,
-            //     value: location.value,
-            //   }))}
-    //         />
-    //       </Flex>
-    //       <Flex
-    //         direction={"column"}
-    //         height={"auto"}
-    //         alignItems={"none"}
-    //         justifyContent={"none"}
-    //       >
-    //         <label style={{ fontSize: "20px" }}>Capacidad</label>
-    //         <Input
-    //           style={{ height: "10px" }}
-    //           input={{ type: "number", min: 1, }}
-    //         ></Input>
-    //         {<p style={{color:"red"}}>El nombre no debe estar vacío</p>}
-    //       </Flex>
-    //       <Flex
-    //         direction={"column"}
-    //         height={"auto"}
-    //         alignItems={"none"}
-    //         justifyContent={"none"}
-    //       >
-    //         <label style={{ fontSize: "20px" }}>Elementos</label>
-    //         <Input
-    //           style={{ height: "10px" }}
-    //           input={{ type: "number", min: 1}}
-    //         ></Input>
-    //         { <p style={{color:"red"}}>El nombre no debe estar vacío</p>}
-    //       </Flex>
-          // <Flex
-          //   direction={"column"}
-          //   height={"auto"}
-          //   alignItems={"none"}
-          //   justifyContent={"none"}
-          // >
-          //   <label style={{ fontSize: "20px" }}>Tipologia</label>
-          //   <Select
-          //     onChange={selectTipologyHandler}
-          //     defaultValue={{ label: tipologies[0].name, value: tipologies[0].value }}
-          //     noOptionsMessage={() => "No se encontraron carreras "}
-          //     className={style.select}
-          //     options={tipologies.map((tipology) => ({
-          //       label: tipology.name,
-          //       value: tipology.value,
-          //     }))}
-          //   />
-          // </Flex>
-    //       <Flex width>
-    //         <Button inLineStyle={{ width: "120px", height: "40px", margin: "10px", backgroundColor: "blue" }} onClick={createClassroomHandler}>
-    //           Guardar
-    //         </Button>
-    //         <Button inLineStyle={{ width: "120px", height: "40px", margin: "10px" }} onClick={()=> navigate("/asignaturas") }>
-    //           Cancelar
-    //         </Button>
-    //       </Flex>
-    //     </Flex>
-    //   </Flex>
-    // </Flex>
     );
 };
