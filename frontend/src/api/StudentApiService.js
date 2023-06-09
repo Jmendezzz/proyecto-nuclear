d99281402702a1b278e72d52b31c1f93a9fc0bd8
santiCardona <sn09pro@gmail.com>
2023-06-08 23:29:25 -0500

    feat(student):change enpoint

--- frontend/src/api/StudentApiService.js
+++ frontend/src/api/StudentApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const api = axios.create({baseURL:"http://localhost:8080"});
 export const getStudents = () => api.get("/students");
-export const saveStudent = (student) =>api.post("/students",student);
+export const saveStudent = (student) =>api.post("/students/create",student);
 export const getStudentById = (id) => api.get(`/students/${id}`);
-export const updateStudent = (studentUpdated) => api.put("/students",studentUpdated);
-export const deleteStudentById = (id) => api.delete(`/students/${id}`);
+export const updateStudent = (studentUpdated) => api.put("/students/update",studentUpdated);
+export const deleteStudentById = (id) => api.delete(`/students/delete/${id}`);
