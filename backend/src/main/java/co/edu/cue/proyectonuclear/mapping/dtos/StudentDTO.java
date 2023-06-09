a7976e154b91e8cd293905edf5fc9aab4b4c234d
santiCardona <sn09pro@gmail.com>
2023-06-08 22:23:15 -0500

    feat(student):validation from DTO

--- backend/src/main/java/co/edu/cue/proyectonuclear/mapping/dtos/StudentDTO.java
+++ backend/src/main/java/co/edu/cue/proyectonuclear/mapping/dtos/StudentDTO.java
@@ -2,16 +2,26 @@ package co.edu.cue.proyectonuclear.mapping.dtos;
 
 import co.edu.cue.proyectonuclear.domain.entities.Subject;
 import co.edu.cue.proyectonuclear.domain.enums.Career;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 public record StudentDTO(
         Long id,
+        @NotNull
         String nid,
+        @NotNull
         String name,
+        @NotNull
         String lastName,
+        @NotNull
         String email,
+        @NotNull
         Career career,
+        @NotNull
+        @Min(value = 1)
         Integer semester,
+        @NotNull
         List<Subject> subjects) {
 }
