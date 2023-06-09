89d25c3453e179d324d6107288c2d4ccbe93094f
samuel <samuelsalcedoramirez@gmail.com>
2023-06-09 06:27:38 -0500

    feature(classroom):implemented search

--- frontend/src/components/classroom/Classroom.jsx
+++ frontend/src/components/classroom/Classroom.jsx
@@ -17,18 +17,41 @@ import Swal from "sweetalert2";
 
 
 
+
+/**
+ * La función reformatElement toma el valor de un elemento y devuelve su nombre correspondiente de un
+ * lista predefinida de elementos.
+ * @returns La función `reformatElement` toma un argumento `element` y busca un objeto en un
+ * matriz llamada `elementos` que tiene una propiedad `valor` igual al argumento `elemento`. Si tal
+ * se encuentra el objeto, la función devuelve el valor de su propiedad `nombre`. De lo contrario, una cadena vacía
+ * es regresado.
+ */
 const reformatElement=(element)=>{
 
   const foundElement = elements.find((e) => e.value === element);
   return foundElement ? foundElement.name : "";
  }
 
+
+/**
+  * La función reformatTipology toma un valor de tipología y devuelve su nombre correspondiente de un
+  * lista predefinida de tipologías.
+  * @returns La función `reformatTipology` toma un parámetro `tipología` y devuelve el correspondiente
+  * Propiedad `nombre` de la matriz `tipologías` si se encuentra una propiedad `valor` coincidente. Si no hay coincidencia
+  * encontrado, se devuelve una cadena vacía.
+  */
  const reformatTipology=(tipology)=>{
 
   const foundTipology = tipologies.find((e) => e.value === tipology);
   return foundTipology ? foundTipology.name : "";
   }
+  
 
+
+/**
+ * El código anterior define dos funciones para mostrar alertas de éxito y error utilizando la biblioteca Swal
+ * en JavaScript Reaccionar.
+ */
   const succesResponseAlert= (response)=>{
     Swal.fire(
       'Eliminado!',
@@ -48,6 +71,9 @@ const reformatElement=(element)=>{
   }
 
 export const Classroom = () => {
+
+/* El código define tres variables de estado usando el gancho `useState`: `classroom`, `currentPage`,
+ y `aulasCambio`. */
   const [classroom, setClassroom] = useState([]);
 
   const [currentPage, setCurrentPage] = useState(1);
@@ -62,10 +88,23 @@ export const Classroom = () => {
       .catch((error) => console.error(error));
       setClassroomsChange(false);
   }, [classroomsChange]);
+
+
+  //-------------------------------------------------------------------------
+
   const lastClassroomIndex = currentPage * classroomsPerPage;
   const firstClassroomIndex = lastClassroomIndex - classroomsPerPage;
   let currentClassroom = classroom.slice(firstClassroomIndex, lastClassroomIndex);
 
+/* El código define una función llamada `deleteClassroomHandler` que toma un parámetro `id`. Este
+La función se usa para manejar la eliminación de un salón de clases llamando a la función `deleteClassroomById`
+del módulo `ClassroomApiService`. Antes de eliminar el aula, aparece un cuadro de diálogo de confirmación.
+se muestra usando la función `Swal.fire` de la biblioteca SweetAlert2. Si el usuario confirma la
+eliminación, se llama a la función `deleteClassroomById` y si la eliminación es exitosa, un éxito
+La alerta se muestra usando la función `succesResponseAlert`. Si hay un error durante la
+eliminación, se muestra una alerta de error utilizando la función `errorResponseAlert`. Finalmente, el
+Se llama a la función `setClassroomsChange` para actualizar el estado de la variable `classroomsChange`,
+que desencadena una nueva representación del componente. */
   const navigate = useNavigate();
   const deleteClassroomHandler = (id) => {
     Swal.fire({
@@ -93,7 +132,15 @@ export const Classroom = () => {
       })
 
     }
+    //-------------------------------------------------------------------------
    
+
+/* Este código define una funcionalidad de búsqueda para el componente Classroom. Crea una variable de estado.
+  `buscar` y una función `searchHandler` que actualiza el estado de `búsqueda` con el valor del
+  campo de entrada. Luego, comprueba si la variable `buscar` no es una cadena vacía y filtra la
+  matriz `currentClassroom` basada en si la propiedad `name` de cada aula incluye el
+  Cadena de `búsqueda` (no distingue entre mayúsculas y minúsculas). Esto permite al usuario buscar un aula específica por
+  nombre. */
     const [search, setSearch] = useState("");
     const searchHandler = (event)=>{
       setSearch(event.target.value);
