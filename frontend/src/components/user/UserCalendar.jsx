2ebdebcbb99cd62e78023f4b37718e6f8904ccf0
Y00w1 <dgomez1019@cue.edu.co>
2023-06-12 21:31:10 -0500

    feat(calendar): Setting the endpoint to receive the courses

--- frontend/src/components/user/UserCalendar.jsx
+++ frontend/src/components/user/UserCalendar.jsx
@@ -1,64 +1,34 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../UI/headers/Header";
 import { Calendar } from "../calendar/Calendar";
 import style from "./User.module.css"
+import { getCoursesByProfessorId } from "../../api/CourseApiService";
+import { useAuth } from "../../context/AuthContext";
+import { Loading } from "../../UI/loading/Loading";
 
 export const UserCalendar = () => {
-    const course = [{
-        id:1,
-        professor: {
-            id: 1,
-            nid: 1234,
-            name: "pepe",
-            lastName: "caro",
-            email:"safa@dasda.com"
-        },
-        subject: {
-            id:1,
-            name:"calculus",
-            career: "software",
-            academicHours:96,
-            period: "TRIMESTRAL",
-            semester: 1,
-            credits: 3
-        },
-        students: [{
-            id:1,
-            name:"pepito"
-        }],
-        courseSchedule: [
-            {
-                id: 1,
-                classroom: {
-                    id:1,
-                    name:"203"
-                },
-                day:"FRIDAY",
-                timeSlot: {
-                    startTime: "16:00:00",
-                    endTime: "19:00:00"
-                }
-            },
-            {
-                id: 1,
-                classroom: {
-                    id:1,
-                    name:"203"
-                },
-                day:"THURSDAY",
-                timeSlot: {
-                    startTime: "16:00:00",
-                    endTime: "19:00:00"
-                }
-            }
-        ]
-    }]
-    return (
+    const {userId} = useAuth();
+    const [courses, setCourses] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect (() =>{
+        getCoursesByProfessorId(userId)
+        .then((response)=>{
+            setCourses(response);
+            setIsLoading(false);
+        })
+        .catch( (error) => console.log(error) );
+    })
+
+    return  isLoading ? (
+        <Loading />
+    ):(
         <div className={style["container"]}>
             <Header>
                 <h2 style={{ fontSize: "60px" }}>Horario</h2>
             </Header>
             <div className={style["main-container"]}>
-                <Calendar data={course} />
+                <Calendar data={courses} />
             </div>
         </div>
     );
