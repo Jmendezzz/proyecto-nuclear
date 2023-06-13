9e8a76cbe3d201a6a3da5a3acbbf71f887240f7a
Y00w1 <dgomez1019@cue.edu.co>
2023-06-13 05:50:22 -0500

    fix(professor): Anottations for validations in professor and student

--- backend/src/main/java/co/edu/cue/proyectonuclear/mapping/dtos/CreateProfessorRequestDTO.java
+++ backend/src/main/java/co/edu/cue/proyectonuclear/mapping/dtos/CreateProfessorRequestDTO.java
@@ -1,16 +1,21 @@
 package co.edu.cue.proyectonuclear.mapping.dtos;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 
 import java.util.List;
 
 public record CreateProfessorRequestDTO(
-        //Long id,
+        @Size(message = "El nid debe tener una longitud entre 7 y 10",  min = 7, max = 10)
         String nid,
+        @NotNull
         String name,
+        @NotNull
         String lastName,
-        @Email(message = "The email should be valid")
+        @NotNull
+        @Email(message = "El email debe ser válido")
         String email,
         List<CreateProfessorRequestSubjectDTO> subjects
 ) {
--- backend/src/main/java/co/edu/cue/proyectonuclear/mapping/dtos/CreateStudentRequestDTO.java
+++ backend/src/main/java/co/edu/cue/proyectonuclear/mapping/dtos/CreateStudentRequestDTO.java
@@ -2,6 +2,7 @@ package co.edu.cue.proyectonuclear.mapping.dtos;
 
 import co.edu.cue.proyectonuclear.domain.entities.Subject;
 import co.edu.cue.proyectonuclear.domain.enums.Career;
+import jakarta.validation.constraints.*;
 
 import java.util.List;
 /*
@@ -10,12 +11,20 @@ import java.util.List;
 * En el mapper se le asigna la password a la Entidad y el role
 * */
 public record CreateStudentRequestDTO(
-        Long id,
+        @Size(message = "El nid debe tener una longitud entre 7 y 10",  min = 7, max = 10)
         String nid,
+        @NotNull
         String name,
+        @NotNull
+        @Email(message = "El email ingresado debe ser válido")
         String email,
+        @NotNull
         String lastName,
+        @NotNull
         Career career,
+        @NotNull
+        @Min(value = 1, message = "El semestre no es válido")
+        @Max(value = 10, message = "El semestre no es válido")
         Integer semester,
         List<Subject> subjects){
 }
